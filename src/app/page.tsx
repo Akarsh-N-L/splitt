6add178019b9bf3c04fd703a32706b050e1ea46b
Akarsh <akarsh@Akarshs-MacBook-Pro.local>
2025-08-08 00:41:03 +0530

    resetting the changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
@@ -24,6 +32,13 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
             Deploy now
           </a>
           <a
@@ -43,6 +58,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
           Learn
         </a>
         <a
@@ -51,6 +73,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
           Examples
         </a>
         <a
@@ -59,6 +88,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
           Go to nextjs.org →
         </a>
       </footer>
--- src/components/ui/PhoneInput.tsx
+++
@@ -1,5 +0,0 @@
-export default function Layout() {
-    return (
-        <p>Content has to filled for this page</p>
-    );
-}
